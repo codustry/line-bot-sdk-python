5c38021b6389a1fd7d2f53780e3b9a64053ca5b5
Nutchanon Ninyawee <nutchanon@codustry.com>
2021-03-01 15:00:41 +0700

    add async support

--- linebot/webhook.py
+++ linebot/webhook.py
@@ -288,3 +288,65 @@ class WebhookHandler(object):
             return event.__name__
         else:
             return event.__name__ + '_' + message.__name__
+
+class WebhookHandlerV2(WebhookHandler):
+    """Webhook Handler for asynchronous compatible
+
+    Please read https://github.com/line/line-bot-sdk-python#webhookhandler
+    """
+
+    async def handle_async(self, body, signature):
+        """Handle webhook.
+
+        :param str body: Webhook request body (as text)
+        :param str signature: X-Line-Signature value (as text)
+        """
+        payload = self.parser.parse(body, signature, as_payload=True)
+
+        for event in payload.events:
+            func = None
+            key = None
+
+            if isinstance(event, MessageEvent):
+                key = self.__get_handler_key(
+                    event.__class__, event.message.__class__)
+                func = self._handlers.get(key, None)
+
+            if func is None:
+                key = self.__get_handler_key(event.__class__)
+                func = self._handlers.get(key, None)
+
+            if func is None:
+                func = self._default
+
+            if func is None:
+                LOGGER.info('No handler of ' + key + ' and no default handler')
+            else:
+                await self.__invoke_func_async(func, event, payload)
+
+    @classmethod
+    def __invoke_func(cls, func, event, payload):
+
+        (has_varargs, args_count) = cls.__get_args_count(func)
+        if has_varargs or args_count == 2:
+            function_return = func(event, payload.destination)
+        elif args_count == 1:
+            function_return = func(event)
+        else:
+            function_return = func()
+
+        if inspect.iscoroutine(function_return):
+            raise Exception("please use `handle_async()` instead of `handle()` for async support")
+
+    @classmethod
+    async def __invoke_func_async(cls, func, event, payload):
+        (has_varargs, args_count) = cls.__get_args_count(func)
+        if has_varargs or args_count == 2:
+            function_return = func(event, payload.destination)
+        elif args_count == 1:
+            function_return = func(event)
+        else:
+            function_return = func()
+
+        if inspect.iscoroutine(function_return):
+            await function_return
